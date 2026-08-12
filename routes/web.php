e9b7cdeaf403fb1751289a6f26b2fb0fcf0653b7
kzxazka <atqiyaazka96@gmail.com>
2026-08-13 03:14:06 +0700

    Add temporary /test-db-env route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,22 @@ Route::resource('services', ServiceController::class);
 // Core Transactional Modules
 Route::resource('projects', ProjectController::class);
 Route::resource('expenses', ExpenseController::class)->only(['index', 'store', 'destroy']);
+
+// Temporary database environment check route
+Route::get('/test-db-env', function () {
+    return [
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => [
+            'value' => env('DB_USERNAME'),
+            'length' => strlen(env('DB_USERNAME')),
+            'has_whitespace' => preg_match('/\s/', env('DB_USERNAME')) ? true : false,
+        ],
+        'DB_PASSWORD' => [
+            'length' => strlen(env('DB_PASSWORD')),
+            'has_whitespace' => preg_match('/\s/', env('DB_PASSWORD')) ? true : false,
+        ],
+    ];
+});
