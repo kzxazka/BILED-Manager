05587450848f0841e715647acbb15edac8ada146
kzxazka <atqiyaazka96@gmail.com>
2026-08-13 03:15:33 +0700

    Trim database credentials to handle whitespaces and newlines

--- config/database.php
+++ config/database.php
@@ -86,12 +86,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DB_URL') ? trim(env('DB_URL')) : null,
+            'host' => trim(env('DB_HOST', '127.0.0.1')),
+            'port' => trim(env('DB_PORT', '5432')),
+            'database' => trim(env('DB_DATABASE', 'postgres')),
+            'username' => trim(env('DB_USERNAME', 'postgres')),
+            'password' => trim(env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
